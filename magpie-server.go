0b83c67c106d219f2586eeefecf898049cce2dc3
justnom <justnom@users.noreply.github.com>
2015-02-09 17:30:11 -0500

    Added command line flags.
    Added `port` and `apiKey` flags.
    Internalized the upload HTML template - instead of serving from the disk,
    this is so the binary can be distributed as a standalone.

--- magpie-server.go
+++ magpie-server.go
@@ -1,27 +1,46 @@
 package main
 
 import (
+	"flag"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
+
+var (
+	port   = flag.Int("port", 3000, "Listening port")
+	apiKey = flag.String("apiKey", "", "API key for uploading")
+)
+
+func isRequestAuthenticated(r *http.Request) bool {
+	requestApiKey := r.Header.Get("X-Api-Key")
+	return *apiKey == "" || requestApiKey == *apiKey
+}
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		content, err := ioutil.ReadFile("./static/upload.html")
-		if err != nil {
-			log.Fatal("Error reading file: ", err)
-		}
-
+		uploadHtml := `<html>
+		<head><title>Upload file</title></head>
+		<body>
+			<form enctype="multipart/form-data" action="/upload" method="post">
+      			<input type="file" name="file" />
+      			<input type="submit" value="upload" />
+			</form>
+		</body></html>`
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.Write(content)
-
+		w.Write([]byte(uploadHtml))
 		return
 	}
 
 	if r.Method == "POST" {
+		if !isRequestAuthenticated(r) {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
 		r.ParseMultipartForm(10 << 20)
 
 		file, handler, err := r.FormFile("file")
@@ -47,15 +66,18 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Println("Starting magpie-repository server...")
+	flag.Parse()
+
+	*apiKey = strings.TrimSpace(*apiKey)
+	if *apiKey == "" {
+		log.Println("Warning: No `apiKey` flag was given! Anyone can upload files.")
+	}
 
 	http.HandleFunc("/upload", uploadHandler)
-
 	fs := http.FileServer(http.Dir("artifacts"))
 	http.Handle("/artifacts/", http.StripPrefix("/artifacts/", fs))
 
-	log.Println("Listening on port 3000!")
-
+	log.Println("Listening on port", *port)
 	// TODO: Serve HTTPS as well
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(*port), nil))
 }
